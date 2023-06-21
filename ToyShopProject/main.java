11e4a9cc4f915d3a4eebd0dcf4d77be0d8d2cbfe
12MGL <makz.ars@gmail.com>
2023-06-21 18:47:40 +0300

    сделал добавление игрушек в список. ошибки ругаются. подумать ещё над этим

--- ToyShopProject/main.java
+++ ToyShopProject/main.java
@@ -1,5 +1,23 @@
-package ToyShopProject
+package ToyShopProject;
+
+import java.io.FileWriter;
+import java.util.Scanner;
+
 
 public class main(String[] args) {
   //здесь будет основное тело программы - запросы игрушек, выдача результатов
+  Scanner input = new Scanner(System.in);
+  System.out.println("Какое количество видов игрушек находятся в магазине?   ");
+  int countToys = input.nextInt();
+  for (int i = 0; i < countToys; i++) {   //для каждой игрушки конструктором добавляем данные в список
+    System.out.println("введите название игрушки:  ");
+    String name = input.next();
+    System.out.println("введите количество игрушек " + name + ":  ");
+    int count = input.nextInt();
+    System.out.println("введите вес игрушки " + name + ":  ");
+    int weight = input.nextInt();
+    Toy toy = new Toy(i, name, count, weight);    
+    ToyShop.addToy(toy);  //по введённым параметрам добавляем игрушку в список, переходим на следующую итерацию
+  }
+
 }
