5ace50db29b71198b66c6210c8232f853c1cd660
12MGL <makz.ars@gmail.com>
2023-06-21 18:09:46 +0300

    определяем список игрушек

--- ToyShopProject/ToyShop.java
+++ ToyShopProject/ToyShop.java
@@ -1,6 +1,18 @@
 package ToyShopProject;
+
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.List;
+
   //здесь будем описывать все манипуляции с игрушками - методы добавления 
   //новых игрушек, розыгрыш, запись результатов
 public class ToyShop {
-  
+    private List<Toy> toyarray = new ArrayList<Toy>(); //определяем список игрушек
+
+    public void addToy(Toy toy) {
+        toyarray.add(toy);      //добавляем игрушки в список
+    }
+
+
+
 }
