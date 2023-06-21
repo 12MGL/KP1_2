071fc2679c02c62c788ace9daa5688b12ef002f0
12MGL <makz.ars@gmail.com>
2023-06-21 13:28:10 +0300

    добавил параметры в игрушки, и их геты

--- ToyShopProject/Toy.java
+++ ToyShopProject/Toy.java
@@ -1,5 +1,26 @@
 package ToyShopProject;
   //здесь будем описывать игрушки
-public class Toy {
-  
+public class Toy { //основные параметры игрушки
+  private int id; 
+  public String name; 
+  public int count;
+  public int weight;
+}
+
+//геты по всем параметрам
+
+public int getId(){
+  return id;
+}
+
+public int getName(){
+  return name;
+}
+
+public int getCount(){
+  return count;
+}
+
+public int getWeight(){
+  return weight;
 }
